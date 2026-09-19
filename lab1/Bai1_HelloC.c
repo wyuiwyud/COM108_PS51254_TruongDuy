c47c3cc4c6241a59b03a337624f9f4ddba8dba5f
Duyui <duyuice@gmail.com>
2026-09-19 10:18:16 +0700

    update 19-09-2026 10:18

--- lab1/Bai1_HelloC.c
+++ lab1/Bai1_HelloC.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-int main(){
-    printf ("Lê Trường Duy\n");
-    printf ("PS51254\n");
-    return 0;
+int main() {
+  printf("Lê Trường Duy\n");
+  printf("PS51254\n");
+  return 0;
 }
